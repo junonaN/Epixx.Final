f2924a30c951c0927f01a98f379b1a0a620b4e04
Junona N <j.vyushkova@gmail.com>
2014-06-23 14:24:04 +0400

    smthg

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function(){
+
+  	$('#review-slider').bxSlider({
+	  	nextSelector: '#next-review',
+	  	prevSelector: '#prev-review',
+	  	nextText: '',
+	  	prevText: '',
+  		pager: false
+	});
+
+  	$('#clients-slider').bxSlider({
+	  	nextSelector: '#next-client',
+	  	prevSelector: '#prev-client',
+	  	nextText: '',
+	  	prevText: '',
+	  	minSlides: 5,
+  		maxSlides: 5,
+  		slideWidth: 200,
+  		slideMargin: 0,
+  		moveSlides: 2,
+  		pager: false
+	});
+
+  	$('#b-slider').bxSlider({
+	  	nextSelector: '#next-banner',
+	  	prevSelector: '#prev-banner',
+	  	nextText: '',
+	  	prevText: '',
+  		pager: false
+	});
+
+});
